70d6ccb158e36011143a8c6d4ccac377ec31ce68
DELL <DELL@DESKTOP-IQSIC9O>
2020-02-07 14:36:05 +0800

    232

--- test1/src/test1/test1.java
+++ test1/src/test1/test1.java
@@ -2,6 +2,6 @@ package test1;
 
 public class test1 {
       public static void main(String[] args) {
-		
+		System.out.println(1);
 	}
 }
--- test1/src/test1/test2.java
+++ test1/src/test1/test2.java
@@ -2,6 +2,6 @@ package test1;
 
 public class test2 {
       public static void main(String[] args) {
-		
+		System.out.println(2);
 	}
 }
