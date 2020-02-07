19069c4c5d4c83b1078421758b70a9f450d9e154
DELL <DELL@DESKTOP-IQSIC9O>
2020-02-07 15:51:27 +0800

    333

--- test1/src/test1/test1.java
+++ test1/src/test1/test1.java
@@ -4,7 +4,7 @@ public class test1 {
       public static void main(String[] args) {
 		System.out.println(1);
 
-		//222222222222
+		//2222222222222
        //1111111111111111111111111111111111111111
 
 	}
--- test1/src/test1/test2.java
+++ test1/src/test1/test2.java
@@ -3,5 +3,6 @@ package test1;
 public class test2 {
       public static void main(String[] args) {
 		System.out.println(2);
+		//12345
 	}
 }
